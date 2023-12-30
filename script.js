2c0895e25a9df0f0e5e87e0e5caa01877e960395
Mahdi <mahdimey17@gmail.com>
2023-12-30 23:23:47 +0330

    selected dom elements from js

---
+++ script.js
@@ -0,0 +1,4 @@
+const colorInputOne = document.querySelector('#color-a')
+const colorInputTwo = document.querySelector('#color-b')
+const arrowButtons = document.querySelectorAll('.buttons button')
+const resultArea = document.querySelector('#copy')
