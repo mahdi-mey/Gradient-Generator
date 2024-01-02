1ac0a1ad4dff2967ec57f351f4997ca6766df639
Mahdi <mahdimey17@gmail.com>
2024-01-02 16:24:06 +0330

    added functionality to coppy to clippboard

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const colorInputOne = document.querySelector('#color-a')
 const colorInputTwo = document.querySelector('#color-b')
 const arrowButtons = document.querySelectorAll('.buttons button')
 const resultArea = document.querySelector('#copy')
+const codeContainer = document.querySelector('.code')
+
+codeContainer.addEventListener('click', () => {
+    navigator.clipboard.writeText(resultArea.innerHTML)
+})
